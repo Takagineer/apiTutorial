302d4d02757e84be28ce5e095044b951a814690b
Takagineer <itengineer0415@gmail.com>
2022-12-28 07:45:15 +0900

    update/POSTメソッド
    POSTメソッドの際のエラーと正常時の記述

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.post("/booklog", (req, res) => {
+  const booklog = req.body;
+  // ↓エラーの際のレスポンス
+  if (!(booklog.name && booklog.text)) {
+    return res.json({
+      ok: false,
+      error: "invalid parameter",
+    });
+  }
+  //↓エラーが出ない際のレスポンス
+  res.json({
+    ok: true,
+    booklog: booklog,
+  });
+});
+
 app.listen(port, () => {
   console.log("listenできてます");
 });
